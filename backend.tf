9ef840eaca359c8f2515a573ea6b9cf295f55e61
justbeingsenat <michelagbobli@gmail.com>
2025-05-05 15:07:04 -0400

    added real bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket  = "replace here with your bucket name"
+    bucket  = "ksena-bucket"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
